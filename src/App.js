83ca33f71d127dfe1b237a24afae1f4d73e65688
ErnestJo <smartlabstech@yahoo.com>
2022-05-04 11:30:44 +0300

    Navbar setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Navbar from './components/layout/Navbar';
+
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <button className="btn">click me</button>
-    </div>
+    <Router>
+     <div className='flex flex-col justify-between h-screen'>
+       <Navbar />
+
+       <main>
+         Content
+       </main>
+     </div>
+    </Router>
   );
 }
 
